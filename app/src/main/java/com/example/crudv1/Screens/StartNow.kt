e86de64715578626e6497a68ed0ece484a9b56bc
daniwete <danielmartinezhuete@gmail.com>
2024-01-23 13:43:10 +0100

    aaa

--- app/src/main/java/com/example/crudv1/Screens/StartNow.kt
+++ app/src/main/java/com/example/crudv1/Screens/StartNow.kt
@@ -2,8 +2,12 @@ package com.example.crudv1.Screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -23,6 +27,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -31,61 +36,72 @@ import java.time.format.TextStyle
 
 @Composable
 fun StartNow(navController: NavHostController) {
-
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
-
-    ){
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
             text = "MERRYSERVICE",
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-                .padding(55.dp),
             style = androidx.compose.ui.text.TextStyle(
                 color = Color.White,
                 fontSize = 36.sp,
                 fontWeight = FontWeight.Bold
-            )
+            ),
+            modifier = Modifier.padding(bottom = 16.dp)
         )
-            Image(
-                painter = painterResource(id = R.drawable.merryservice2),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(400.dp)
-                    .align(Alignment.Center)
-                    .offset(y = (-100).dp),
-                )
+        Image(
+            painter = painterResource(id = R.drawable.merryservice2),
+            contentDescription = null,
+            modifier = Modifier
+                .size(400.dp)
+                .padding(bottom = 56.dp),
+            contentScale = ContentScale.Crop
+        )
         Button(
             onClick = { /* Acción al hacer clic en el botón */ },
             modifier = Modifier
-
-                .align(Alignment.BottomCenter)
-                .padding(bottom = 126.dp)
                 .height(100.dp)
                 .width(300.dp)
                 .background(color = Color.Black, shape = RoundedCornerShape(12.dp)),
-                    colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.White,
-                    contentColor = Color.White
-        )
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White,
+                contentColor = Color.White
+            ),
+            shape = RoundedCornerShape(12.dp)
         ) {
             Text(
                 text = "START NOW!!!",
                 color = Color.Black,
-                fontWeight = FontWeight.Bold ,
+                fontWeight = FontWeight.Bold,
                 style = androidx.compose.ui.text.TextStyle(
-                fontSize = 26.sp,
-                fontWeight = FontWeight.Bold
+                    fontSize = 26.sp,
+                    fontWeight = FontWeight.Bold
+                )
             )
-            )
-
-
         }
-        Text(
-            text=""
-        )
-
+        Spacer(modifier = Modifier.size(66.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Do you have an account?",
+                fontSize = 20.sp,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(
+                text = "Log in-->",
+                color = Color.Red,
+                fontSize = 20.sp,
+                modifier = Modifier
+                    .clickable {
+                        navController.navigate("") // Ajusta la acción de navegación según sea necesario
+                    }
+            )
+        }
     }
 }
