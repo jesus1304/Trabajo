68a24923cd3dda52d6ae22d3fc06b57bda653714
daniwete <danielmartinezhuete@gmail.com>
2024-01-25 10:49:18 +0100

    aaad

--- app/src/main/java/com/example/crudv1/Screens/StartNow.kt
+++ app/src/main/java/com/example/crudv1/Screens/StartNow.kt
@@ -17,7 +17,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -26,6 +28,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -68,59 +71,64 @@ fun StartNow(navController: NavHostController) {
                 .width(400.dp)
                 .height(400.dp)
                 .background(color = Color.Black, shape = RoundedCornerShape(82.dp))
-                .background(
-                    color = Color.Black,
-                    shape = RoundedCornerShape(72.dp)
-                )
                 .border(
-                    width = 20.dp,
+                    width = 4.dp,
                     color = Color.White,
                     shape = RoundedCornerShape(82.dp)
                 )
-                .wrapContentWidth(Alignment.CenterHorizontally)
+                .clip(RoundedCornerShape(72.dp))
                 .padding(horizontal = 16.dp)
+
+                .clip(CircleShape) // Aplica forma de cilindro al Box
+
         ) {
-            Button(
-                onClick = { navController.navigate("ClienteGuardar") },
-                modifier = Modifier
-                    .height(100.dp)
-                    .width(300.dp)
-                    .background(color = Color.Black, shape = RoundedCornerShape(12.dp)),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.White,
-                    contentColor = Color.White
-                ),
-                shape = RoundedCornerShape(12.dp)
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
             ) {
-                Text(
-                    text = "START NOW!!!",
-                    color = Color.Black,
-                    fontWeight = FontWeight.Bold,
-                    style = androidx.compose.ui.text.TextStyle(
-                        fontSize = 26.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                )
-            }
-            Spacer(modifier = Modifier.size(66.dp))
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "Do you have an account?",
-                    fontSize = 20.sp,
-                    color = Color.White
-                )
-                Spacer(modifier = Modifier.size(8.dp))
-                Text(
-                    text = "Log in-->",
-                    color = Color.Red,
-                    fontSize = 20.sp,
+                Button(
+                    onClick = { navController.navigate("ClienteGuardar") },
                     modifier = Modifier
-                        .clickable {
-                            navController.navigate("") // Ajusta la acción de navegación según sea necesario
-                        }
-                )
+                        .height(100.dp)
+                        .width(300.dp)
+                        .background(color = Color.Black, shape = RoundedCornerShape(12.dp)),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.White,
+                        contentColor = Color.White
+                    ),
+                    shape = RoundedCornerShape(12.dp)
+                ) {
+                    Text(
+                        text = "START NOW!!!",
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold,
+                        style = androidx.compose.ui.text.TextStyle(
+                            fontSize = 26.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+                }
+                Spacer(modifier = Modifier.size(66.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Do you have an account?",
+                        fontSize = 20.sp,
+                        color = Color.White
+                    )
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Text(
+                        text = "Log in-->",
+                        color = Color.Red,
+                        fontSize = 20.sp,
+                        modifier = Modifier
+                            .clickable {
+                                navController.navigate("") // Ajusta la acción de navegación según sea necesario
+                            }
+                    )
+                }
             }
         }
     }
